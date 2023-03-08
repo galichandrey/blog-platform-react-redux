cdf4be3708899477ca57a16472f482f44eabf447
cehcop <galich.andrey@gmail.com>
2023-03-09 02:29:50 +0700

    Improvements: react router, article list, full article, pagination

--- src/index.jsx
+++ src/index.jsx
@@ -13,8 +13,8 @@ const rootReducer = combineReducers({
 
 const store = createStore(
   rootReducer,
-  // compose(applyMiddleware(thunk))
-  compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+  compose(applyMiddleware(thunk))
+  // compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
